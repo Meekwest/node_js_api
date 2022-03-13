029656167217dfe4ccf1fc6943ab36ec749aeb39
Meekwest <meekwest@mail.ru>
2022-03-13 17:53:14 +0300

    UserService configured

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { IExceptionFilter } from './errors/exception.filter.interface';
 import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
+import { UserService } from './users/users.service';
+import { IUserService } from './users/user.service.interface';
+import { IUserController } from './users/users.controller.interface';
 
 export interface IBootstrapReturn {
 	appContainer: Container;
@@ -15,7 +18,8 @@ export interface IBootstrapReturn {
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.ILogger).to(LoggerService);
 	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
-	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserService).to(UserService);
 	bind<App>(TYPES.Application).to(App);
 });
 
