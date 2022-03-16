2649392295149f33a7603fc33781e0afbe645044
Meekwest <meekwest@mail.ru>
2022-03-16 13:31:14 +0300

    JWT, Middleware JWT added

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import { json } from 'body-parser';
 import 'reflect-metadata';
 import { IConfigService } from './config/config.service.interface';
 import { PrismaService } from './database/prisma.service';
+import { AuthMiddleware } from './common/auth.middleware';
 
 @injectable()
 export class App {
@@ -30,6 +31,8 @@ export class App {
 
 	useMiddleWare(): void {
 		this.app.use(json());
+		const authMiddleware = new AuthMiddleware(this.configService.get('SECRET'));
+		this.app.use(authMiddleware.execute.bind(authMiddleware));
 	}
 
 	useRoutes(): void {
--- src/users/users.controller.interface.ts
+++ src/users/users.controller.interface.ts
@@ -5,4 +5,5 @@ import { UserRegisterDto } from './dto/user-register.dto';
 export interface IUserController {
 	login: (req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction) => void;
 	register: (req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction) => void;
+	info: (req: Request, res: Response, next: NextFunction) => void;
 }
